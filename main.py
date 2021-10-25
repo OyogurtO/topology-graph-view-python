f4ab598a45909b6f356530ae6171b63deea82f00
Jingchun.Zhou <jingchun.zhou@kuka.com>
2021-10-25 17:25:21 +0800

    add tests data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from src.view import topology_view
+from src.view import topology_graph_view
 from src.io import parse_or_input_file_name, load_json_data
 
 
@@ -8,7 +8,7 @@ def main(argv):
     file_name = parse_or_input_file_name.parse_file_name(argv)
     data = load_json_data.load(file_name)
     print(len(data))
-    topology_view.view(data['floorList'][0]['nodeList'], data['floorList'][0]['edgeList'], {})
+    topology_graph_view.view(data['floorList'][0]['nodeList'], data['floorList'][0]['edgeList'], {})
 
 
 if __name__ == '__main__':
