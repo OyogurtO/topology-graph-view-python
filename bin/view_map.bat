2d093ff53d5e37e88bae30bedb18c80973e2047f
Jingchun.Zhou <jingchun.zhou@kuka.com>
2021-12-13 13:14:13 +0800

    update cmd script, auto close cmd window after executing python

--- bin/view_map.bat
+++ bin/view_map.bat
@@ -1,7 +1,16 @@
 @echo off
+:: if running as vbscript, goto executing python
+if "%1" == "vbs" goto begin
+
 :: show *.json files which are candidate maps
 for %%f in (*.json) do (
     echo %%f
 )
-python D:\code\github\topology-graph-view-python\main.py
+echo please enter file name:
+set /p file=
 
+:: restart as vbscript, and close current cmd window
+mshta vbscript:createobject("wscript.shell").run("%~nx0 vbs",0)(window.close)&&exit
+
+:begin
+python D:\code\github\topology-graph-view-python\main.py %file%
