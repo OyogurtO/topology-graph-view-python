b751c6bbe279b8427d46d35e1e797e3b8c877502
Jingchun.Zhou <jingchun.zhou@kuka.com>
2022-09-23 11:04:23 +0800

    support utf-8

--- src/io/load_json_data.py
+++ src/io/load_json_data.py
@@ -2,7 +2,7 @@ import json
 
 
 def load(file_name):
-    with open(file_name, 'r') as load_f:
+    with open(file_name, 'r', encoding='utf-8') as load_f:
         data = json.load(load_f)
         # print(data)
         return data
